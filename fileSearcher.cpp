6d884e7600f36c247437b08aa402c913d2a1c72a
XUAXUQAQ <xuanxusb@outlook.com>
2020-02-13 22:23:01 +0800

    减少内存占用

--- fileSearcher.cpp
+++ fileSearcher.cpp
@@ -4,30 +4,18 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 using namespace std;  
-//#define TEST
 
 
-extern "C" __declspec(dllexport) void addIgnorePath(const char* path);
-extern "C" __declspec(dllexport) void searchFiles(const char* path, const char* exd);
-extern "C" __declspec(dllexport) void searchFilesIgnoreSearchDepth(const char* path, const char* exd);
-extern "C" __declspec(dllexport) void setSearchDepth(int i);
-extern "C" __declspec(dllexport) char* getResult();
-extern "C" __declspec(dllexport) void deleteResult();
-extern "C" __declspec(dllexport) bool ResultReady();
-extern "C" __declspec(dllexport) void clearResults();
 
 vector<string> ignorePath;
-vector<string> files;  
 int searchDepth;
+char* searchPath;
 string results;
-char* cstr;
-bool isResultReady = false;
 
-bool ResultReady();
+
 void searchFiles(const char* path, const char* exd);
-char* getResult();
-void deleteResult();
 void addIgnorePath(const char* path);
 int count(string path, string pattern);
 bool isSearchDepthOut(string path);
@@ -36,19 +24,12 @@ void search(string path, string exd);
 void searchIgnoreSearchDepth(string path, string exd);
 void clearResults();
 
-__declspec(dllexport) void searchFilesIgnoreSearchDepth(const char* path, const char* exd){
+void searchFilesIgnoreSearchDepth(const char* path, const char* exd){
     string file(path);
     string suffix(exd);
-    isResultReady = false;
     cout << "start search without searchDepth" << endl;
     searchIgnoreSearchDepth(file, suffix);
     cout <<"end search without searchDepth" <<endl;
-    isResultReady = true;
-}
-
-__declspec(dllexport) void clearResults(){
-    results.clear();
-    files.clear();
 }
 
 
@@ -80,7 +61,8 @@ void searchIgnoreSearchDepth(string path, string exd){
             if ((fileinfo.attrib &  _A_SUBDIR))  
             {  
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0){
-                    files.push_back(pathName.assign(path).append("\\").append(fileinfo.name));  
+                    results.append(pathName.assign(path).append("\\").append(fileinfo.name));
+                    results.append("\n");
                     bool Ignore = isIgnore(path);
                     if (!Ignore){
                         searchIgnoreSearchDepth(pathName.assign(path).append("\\").append(fileinfo.name), exd); 
@@ -91,51 +73,32 @@ void searchIgnoreSearchDepth(string path, string exd){
             else  
             {  
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)  
-                    files.push_back(pathName.assign(path).append("\\").append(fileinfo.name));  
+                    results.append(pathName.assign(path).append("\\").append(fileinfo.name));
+                    results.append("\n");
             }  
         } while (_findnext(hFile, &fileinfo) == 0);  
         _findclose(hFile);  
     }
 }
 
-__declspec(dllexport) bool ResultReady(){
-    return isResultReady;
-}
 
-__declspec(dllexport) void searchFiles(const char* path, const char* exd){
+void searchFiles(const char* path, const char* exd){
     string file(path);
     string suffix(exd);
-    isResultReady = false;
     cout << "start Search" << endl;
     search(file, exd);
     cout << "end Search" << endl;
-    isResultReady = true;
     //cout << isResultReady << endl;
 }
 
-__declspec(dllexport) char* getResult(){
-    int size = files.size();  
-    for (int i = 0; i < size; i++)  
-    { 
-        results.append(files[i]);
-        results.append("\n");
-    } 
-    cstr = new char[results.length() +1];
-    strcpy(cstr, results.c_str());
-    return cstr;
-}
 
-__declspec(dllexport) void deleteResult(){
-    delete[] cstr;
-}
-
-__declspec(dllexport) void addIgnorePath(const char* path){
+void addIgnorePath(const char* path){
     string str(path);
     transform(str.begin(), str.end(), str.begin(), ::tolower);
     ignorePath.push_back(str);
 }
 
-__declspec(dllexport) void setSearchDepth(int i){
+void setSearchDepth(int i){
     searchDepth = i;
 }
 
@@ -202,39 +165,85 @@ void search(string path, string exd)
             if ((fileinfo.attrib &  _A_SUBDIR))  
             {  
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0){
-                    files.push_back(pathName.assign(path).append("\\").append(fileinfo.name));  
+                    results.append(pathName.assign(path).append("\\").append(fileinfo.name));
+                    results.append("\n");
+
                     bool SearchDepthOut = isSearchDepthOut(path);
                     bool Ignore = isIgnore(path);
                     bool result = !Ignore && !SearchDepthOut;
                     if (result){
-                        search(pathName.assign(path).append("\\").append(fileinfo.name), exd); 
-                        //cout << isResultReady << endl;
+                        search(pathName.assign(path).append("\\").append(fileinfo.name), exd);                    
                     } 
                 }
             }  
             else  
             {  
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)  
-                    files.push_back(pathName.assign(path).append("\\").append(fileinfo.name));  
+                    results.append(pathName.assign(path).append("\\").append(fileinfo.name));
+                    results.append("\n");
             }  
         } while (_findnext(hFile, &fileinfo) == 0);  
         _findclose(hFile);  
     }
 }  
   
-#ifdef TEST
-int main()  
+
+int main(int argc, char* argv[])  
 {  
-    char *result;
-    addIgnorePath("C:\\Windows");
-    setSearchDepth(6);
-    clearResults();
-    //searchFiles("D:", "*");  
-    searchFilesIgnoreSearchDepth("C:\\Users\\13927\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu", "*");
-    result = getResult();
-    cout << result << endl;
-    deleteResult();
-    system("pause");
-    return 0;
+    char searchPath[260];
+    char searchDepth[50];
+    char ignorePath[3000];
+    char output[260];
+    char isIgnoreSearchDepth[2];
+    ofstream outfile;
+    if (argc == 6){
+        strcpy(searchPath, argv[1]);
+        strcpy(searchDepth, argv[2]);
+        setSearchDepth(atoi(searchDepth));
+        strcpy(ignorePath, argv[3]);
+        strcpy(output, argv[4]);
+        strcpy(isIgnoreSearchDepth, argv[5]);
+        cout << "searchPath:" << searchPath <<endl;
+        cout << "searchDepth:" << searchDepth <<endl;
+        cout << "ignorePath:" << ignorePath <<endl;
+        cout << "output:" << output <<endl;
+        cout << "isIgnoreSearchDepth:" << isIgnoreSearchDepth << endl; 
+        char *p = NULL;
+        char *_ignorepath = ignorePath;
+        p = strtok(_ignorepath, ",");
+        if (p != NULL){
+            addIgnorePath(p);
+            cout << "adding ignorePath:" << p << endl;
+        }
+        
+        while (p != NULL){
+            p = strtok(NULL, ",");
+            if (p != NULL){
+                addIgnorePath(p);
+                cout << "adding ignorePath:" << p << endl;
+            }
+        }
+        clock_t startTime,endTime;
+        startTime = clock();
+        if (atoi(isIgnoreSearchDepth) == 1){
+            cout << "ignore searchDepth!!!" << endl;
+            searchFilesIgnoreSearchDepth(searchPath, "*");
+            //将结果写入文件                    
+            outfile.open(output);               
+            outfile << results << endl;
+            outfile.close();
+        }else{
+            cout << "normal search" << endl;
+            searchFiles(searchPath, "*");  
+            //将结果写入文件                    
+            outfile.open(output);               
+            outfile << results << endl;
+            outfile.close();
+        }
+        endTime = clock();
+        cout << "Totle Time : " <<(double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+        return 0;
+    }else{
+        cout << "args error" << endl;
+    } 
 }  
-#endif
