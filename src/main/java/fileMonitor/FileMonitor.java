ad964b0b04d257e981637ac714d8ce71713f2427
XUAXUQAQ <xuanxusb@outlook.com>
2020-02-27 13:39:38 +0800

    添加复制路径功能

--- src/main/java/fileMonitor/FileMonitor.java
+++ src/main/java/fileMonitor/FileMonitor.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 
 public interface FileMonitor extends Library {
-    File dll = new File("./user/fileMonitor.dll");
+    File dll = new File("user/fileMonitor.dll");
     FileMonitor INSTANCE = (FileMonitor) Native.loadLibrary(dll.getAbsolutePath(), FileMonitor.class);
 
     void monitor(String path, String output, String closePosition);
