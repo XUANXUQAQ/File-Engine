b13056bdf01f1f294ff2489e4e21026d4713813c
XUAXUQAQ <xuanxusb@outlook.com>
2020-03-02 13:56:52 +0800

    降低内存占用

---
+++ src/main/java/hotkeyListener/HotkeyListener.java
@@ -0,0 +1,16 @@
+package hotkeyListener;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public interface HotkeyListener extends Library {
+    HotkeyListener INSTANCE = Native.load("hotkeyListener", HotkeyListener.class);
+
+    void registerHotKey(int key1, int key2, int key3);
+
+    boolean getKeyStatus();
+
+    void startListen();
+
+    void stopListen();
+}
--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -104,9 +104,9 @@ public class MainClass {
         try {
             System.setProperty("sun.java2d.noddraw", "true");
             System.setProperty("jna.library.path", new File("user").getAbsolutePath() + "\\");
-            /*org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
-            UIManager.put("RootPane.setupButtonVisible", false);*/
-            WebLookAndFeel.install();
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+            UIManager.put("RootPane.setupButtonVisible", false);
+            WebLookAndFeel.initializeManagers();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -232,6 +232,8 @@ public class MainClass {
         InputStream fileMonitor86Dll = MainClass.class.getResourceAsStream("/win32-x86/fileMonitor86.dll");
         InputStream getAscII64Dll = MainClass.class.getResourceAsStream("/win32-x86-64/getAscII64.dll");
         InputStream getAscII86Dll = MainClass.class.getResourceAsStream("/win32-x86/getAscII86.dll");
+        InputStream hotkeyListener64Dll = MainClass.class.getResourceAsStream("/win32-x86-64/hotkeyListener64.dll");
+        InputStream hotkeyListener86Dll = MainClass.class.getResourceAsStream("/win32-x86/hotkeyListener86.dll");
 
         boolean is64Bit = name.contains("x64");
 
@@ -244,6 +246,10 @@ public class MainClass {
             if (!target.exists()) {
                 copyFile(getAscII64Dll, target);
             }
+            target = new File("user/hotkeyListener.dll");
+            if (!target.exists()) {
+                copyFile(hotkeyListener64Dll, target);
+            }
         } else {
             target = new File("user/fileMonitor.dll");
             if (!target.exists()) {
@@ -252,6 +258,10 @@ public class MainClass {
             target = new File("user/getAscII.dll");
             if (!target.exists()) {
                 copyFile(getAscII86Dll, target);
+            }
+            target = new File("user/hotkeyListener.dll");
+            if (!target.exists()) {
+                copyFile(hotkeyListener86Dll, target);
             }
         }
         SettingsFrame.initSettings();
