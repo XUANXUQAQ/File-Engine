7714af937c62b4c3821ee9fc5529dc8f44c140cd
XUAXUQAQ <xuanxusb@outlook.com>
2020-03-27 19:54:51 +0800

    更新版本号

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -18,7 +18,7 @@ import java.util.concurrent.Executors;
 
 
 public class MainClass {
-    public static final String version = "1.8"; //TODO 更改版本号
+    public static final String version = "1.9"; //TODO 更改版本号
     public static boolean mainExit = false;
     public static String name;
     private static Search search = Search.getInstance();
