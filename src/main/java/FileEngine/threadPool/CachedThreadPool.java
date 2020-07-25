1cf4d491f4b5ba99f8af21ef9b74376f4a38ba7c
XUAXUQAQ <xuanxusb@outlook.com>
2020-07-25 12:18:35 +0800

    修改线程池，添加数据库优化提示。

--- src/main/java/FileEngine/threadPool/CachedThreadPool.java
+++ src/main/java/FileEngine/threadPool/CachedThreadPool.java
@@ -1,10 +1,15 @@
 package FileEngine.threadPool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class CachedThreadPool {
-    private final ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+    private final ExecutorService cachedThreadPool = new ThreadPoolExecutor(
+            0,
+            100,
+            60L,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>());
+
 
     private static class CachedThreadPoolBuilder {
         private static final CachedThreadPool INSTANCE = new CachedThreadPool();
