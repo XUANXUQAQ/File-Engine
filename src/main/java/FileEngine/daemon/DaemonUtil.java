aa169265e6f34953d7ef9a5d668b1bc2a4c4e334
XUANXUQAQ <xuanxusb@outlook.com>
2020-08-19 15:10:27 +0800

    防止守护进程重复打开

--- src/main/java/FileEngine/daemon/DaemonUtil.java
+++ src/main/java/FileEngine/daemon/DaemonUtil.java
@@ -1,21 +1,25 @@
 package FileEngine.daemon;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DaemonUtil {
     public static void startDaemon(String currentWorkingDir) {
-        File daemonProcess = new File("user/daemonProcess.exe");
-        if (daemonProcess.canExecute()) {
-            try {
-                String command = daemonProcess.getAbsolutePath();
-                String start = "cmd.exe /c " + command.substring(0, 2);
-                String end = "\"" + command.substring(2) + "\"";
-                String finalCommand = start + end + " \"" + currentWorkingDir + "\"";
-                Runtime.getRuntime().exec(finalCommand, null, daemonProcess.getParentFile());
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            if (!isDaemonExist()) {
+                File daemonProcess = new File("user/daemonProcess.exe");
+                if (daemonProcess.canExecute()) {
+                    String command = daemonProcess.getAbsolutePath();
+                    String start = "cmd.exe /c " + command.substring(0, 2);
+                    String end = "\"" + command.substring(2) + "\"";
+                    String finalCommand = start + end + " \"" + currentWorkingDir + "\"";
+                    Runtime.getRuntime().exec(finalCommand, null, daemonProcess.getParentFile());
+                }
             }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -26,5 +30,18 @@ public class DaemonUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isDaemonExist() throws IOException, InterruptedException {
+        StringBuilder strBuilder = new StringBuilder();
+        Process p = Runtime.getRuntime().exec("tasklist /FI \"IMAGENAME eq " + "daemonProcess.exe" + "\"");
+        p.waitFor();
+        String eachLine;
+        try (BufferedReader buffr = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+            while ((eachLine = buffr.readLine()) != null) {
+                strBuilder.append(eachLine);
+            }
+        }
+        return strBuilder.toString().contains("daemonProcess.exe");
     }
 }
