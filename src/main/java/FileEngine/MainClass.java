54ce7bbf117b2b8f57d0a6faa34d205b14920865
XUANXUQAQ <xuanxusb@outlook.com>
2020-08-23 23:08:23 +0800

    排除Internet download manager对话框和idea的push窗口

--- src/main/java/FileEngine/MainClass.java
+++ src/main/java/FileEngine/MainClass.java
@@ -33,7 +33,7 @@ public class MainClass {
     private static final String IS_NTFS_64_MD_5 = "0b5f7ea2923a42873883a3bcda2bafd2";
     private static final String SQLITE3_64_MD_5 = "658c71b8b93ba4eb5b4936f46a112449";
     private static final String UPDATER_BAT_64_MD_5 = "357d7cc1cf023cb6c90f73926c6f2f55";
-    private static final String GET_HANDLE_64_MD_5 = "04d6e51413311a6d6d63ce04455593b6";
+    private static final String GET_HANDLE_64_MD_5 = "13d4895fd5c5399b8055af7e678a4470";
     private static final String DAEMON_PROCESS_64_MD_5 = "797826e9b5c4ca9ccc3fb90520e5d805";
 
     private static final String SHORTCUT_GENERATOR_MD_5 = "fa4e26f99f3dcd58d827828c411ea5d7";
