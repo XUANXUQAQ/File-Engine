4cffa9401964c926d958914e80850896cb6b7944
XUANXUQAQ <xuanxusb@outlook.com>
2020-09-29 10:33:51 +0800

    优化代码，修改版本号

--- C++/isNTFS/isNTFS/dllmain.cpp
+++ C++/isNTFS/isNTFS/dllmain.cpp
@@ -7,22 +7,6 @@ using namespace std;
 
 extern "C" __declspec(dllexport) bool isDiskNTFS(const char* disk);
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
-}
-
 __declspec(dllexport) bool isDiskNTFS(const char* disk) {
 	char lpRootPathName[20];
 	strcpy_s(lpRootPathName, disk);
