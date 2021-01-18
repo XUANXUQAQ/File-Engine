2d6ac6a23cc66bee98d893d090e13491998161a1
XUANXUQAQ <xuanxusb@outlook.com>
2021-01-18 16:10:14 +0800

    修复下载错误

--- src/main/java/FileEngine/eventHandler/impl/download/StartDownloadEvent.java
+++ src/main/java/FileEngine/eventHandler/impl/download/StartDownloadEvent.java
@@ -2,34 +2,9 @@ package FileEngine.eventHandler.impl.download;
 
 import FileEngine.utils.download.DownloadManager;
 
-import java.io.File;
-import java.io.IOException;
-
 public class StartDownloadEvent extends DownloadEvent {
 
     public StartDownloadEvent(DownloadManager downloadManager) {
         super(downloadManager);
-        try {
-            boolean isFileExist;
-            File dir = new File(downloadManager.savePath);
-            File f = new File(dir, downloadManager.fileName);
-            if (f.exists()) {
-                isFileExist = true;
-            } else {
-                if (dir.exists()) {
-                    isFileExist = f.createNewFile();
-                } else {
-                    if ((isFileExist = dir.mkdirs())) {
-                        isFileExist = f.createNewFile();
-                    }
-                }
-            }
-            if (!isFileExist) {
-                throw new IOException("创建下载任务文件" +
-                        downloadManager.fileName + File.pathSeparatorChar + downloadManager.savePath + "失败");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
