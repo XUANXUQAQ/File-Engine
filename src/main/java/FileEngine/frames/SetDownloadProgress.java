462478857758322b087e02621ca2f97aff6b1a84
XUANXUQAQ <xuanxusb@outlook.com>
2021-02-22 15:34:35 +0800

    修复下载按钮显示与选择插件不同的问题

--- src/main/java/FileEngine/frames/SetDownloadProgress.java
+++ src/main/java/FileEngine/frames/SetDownloadProgress.java
@@ -2,9 +2,9 @@ package FileEngine.frames;
 
 import FileEngine.configs.Enums;
 import FileEngine.eventHandler.EventManagement;
-import FileEngine.utils.TranslateUtil;
 import FileEngine.services.download.DownloadManager;
 import FileEngine.services.download.DownloadService;
+import FileEngine.utils.TranslateUtil;
 
 import javax.swing.*;
 import java.io.File;
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
 
 /**
  * 使用方法：给点击下载的button添加actionListener，用一个AtomicBoolean isDownloadStarted判断当前按钮是点击下载，还是点击取消下载
@@ -28,7 +28,7 @@ public class SetDownloadProgress {
      * @param labelProgress 显示进度的label
      * @param buttonInstall 设置文字为下载还是取消的下载点击按钮
      * @param downloadManager 下载管理类的实例
-     * @param isDownloadStarted 必须为true，在下载失败后会被恢复为false，用于在buttonInstall判断点击时是取消下载还是开始下载
+     * @param func 判断是否需要显示label和button的状态
      * @param successSign 下载成功后创建文件
      * @param currentTaskStr 当前任务的标志
      * @param getSelectedMethod 线程需要从哪个方法获取字符串，当获取的字符串不等于currentTaskStr时，则会停止设置buttonInstall和labelProgress的值
@@ -37,13 +37,12 @@ public class SetDownloadProgress {
     protected static void setProgress(JLabel labelProgress,
                                    JButton buttonInstall,
                                    DownloadManager downloadManager,
-                                   AtomicBoolean isDownloadStarted,
+                                   Supplier<Boolean> func,
                                    File successSign,
                                    String currentTaskStr,
                                    Method getSelectedMethod,
                                    Object invokeMethodObj) {
         try {
-            isDownloadStarted.set(true);
             TranslateUtil translateUtil = TranslateUtil.getInstance();
             DownloadService downloadService = DownloadService.getInstance();
             EventManagement eventManagement = EventManagement.getInstance();
@@ -51,54 +50,51 @@ public class SetDownloadProgress {
             boolean isStarted = true;
             boolean isDownloadStartedSet = false;    //当getSelectedMethod获取到的字符串不等于currentTaskStr，让isDownloadStarted设置为false（只设置一次，之后会由其他线程托管）
             while (isStarted) {
-                if (!eventManagement.isNotMainExit()) {
-                    return;
-                }
-                String taskStrFromMethod = currentTaskStr;
-                if (getSelectedMethod != null) {
-                    taskStrFromMethod = (String) getSelectedMethod.invoke(invokeMethodObj);
-                }
-                if (currentTaskStr.equals(taskStrFromMethod)) {
-                    isDownloadStarted.set(true);
-                    isDownloadStartedSet = false;
-                    double progress = downloadService.getDownloadProgress(downloadManager);
-                    Enums.DownloadStatus downloadStatus = downloadService.getDownloadStatus(downloadManager);
-                    if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_DONE) {
-                        //下载完成，禁用按钮
-                        labelProgress.setText("");
-                        buttonInstall.setText(translateUtil.getTranslation("Downloaded"));
-                        buttonInstall.setEnabled(false);
-                        isDownloadStarted.set(false);
-                        isStarted = false;
-                        if (!successSign.exists()) {
-                            if (!successSign.createNewFile()) {
-                                throw new RuntimeException("创建更新标识符失败");
-                            }
-                        }
-                    } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_ERROR) {
-                        //下载错误，重置button
-                        labelProgress.setText(translateUtil.getTranslation("Download failed"));
-                        buttonInstall.setText(buttonOriginalText);
-                        buttonInstall.setEnabled(true);
-                        isDownloadStarted.set(false);
-                        isStarted = false;
-                    } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_DOWNLOADING) {
-                        //正在下载
-                        labelProgress.setText(translateUtil.getTranslation("Downloading:") + (int) (progress * 100) + "%");
-                        buttonInstall.setText(translateUtil.getTranslation("Cancel"));
-                        buttonInstall.setEnabled(true);
-                    } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_INTERRUPTED) {
-                        //用户自行中断
-                        labelProgress.setText("");
-                        buttonInstall.setText(buttonOriginalText);
-                        buttonInstall.setEnabled(true);
-                        isDownloadStarted.set(false);
-                        isStarted = false;
+                if (func.get()) {
+                    if (!eventManagement.isNotMainExit()) {
+                        return;
                     }
-                } else {
-                    if (!isDownloadStartedSet) {
-                        isDownloadStartedSet = true;
-                        isDownloadStarted.set(false);
+                    String taskStrFromMethod = currentTaskStr;
+                    if (getSelectedMethod != null) {
+                        taskStrFromMethod = (String) getSelectedMethod.invoke(invokeMethodObj);
+                    }
+                    if (currentTaskStr.equals(taskStrFromMethod)) {
+                        isDownloadStartedSet = false;
+                        double progress = downloadService.getDownloadProgress(downloadManager);
+                        Enums.DownloadStatus downloadStatus = downloadService.getDownloadStatus(downloadManager);
+                        if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_DONE) {
+                            //下载完成，禁用按钮
+                            labelProgress.setText("");
+                            buttonInstall.setText(translateUtil.getTranslation("Downloaded"));
+                            buttonInstall.setEnabled(false);
+                            isStarted = false;
+                            if (!successSign.exists()) {
+                                if (!successSign.createNewFile()) {
+                                    throw new RuntimeException("创建更新标识符失败");
+                                }
+                            }
+                        } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_ERROR) {
+                            //下载错误，重置button
+                            labelProgress.setText(translateUtil.getTranslation("Download failed"));
+                            buttonInstall.setText(buttonOriginalText);
+                            buttonInstall.setEnabled(true);
+                            isStarted = false;
+                        } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_DOWNLOADING) {
+                            //正在下载
+                            labelProgress.setText(translateUtil.getTranslation("Downloading:") + (int) (progress * 100) + "%");
+                            buttonInstall.setText(translateUtil.getTranslation("Cancel"));
+                            buttonInstall.setEnabled(true);
+                        } else if (downloadStatus == Enums.DownloadStatus.DOWNLOAD_INTERRUPTED) {
+                            //用户自行中断
+                            labelProgress.setText("");
+                            buttonInstall.setText(buttonOriginalText);
+                            buttonInstall.setEnabled(true);
+                            isStarted = false;
+                        }
+                    } else {
+                        if (!isDownloadStartedSet) {
+                            isDownloadStartedSet = true;
+                        }
                     }
                 }
                 TimeUnit.MILLISECONDS.sleep(50);
