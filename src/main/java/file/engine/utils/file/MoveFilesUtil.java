d5303b80c25c55d718a6cfd04351b1d662c2da1c
xuanxu <xuanxusb@outlook.com>
2021-07-09 13:19:29 +0800

    添加翻译

--- src/main/java/file/engine/utils/file/MoveFilesUtil.java
+++ src/main/java/file/engine/utils/file/MoveFilesUtil.java
@@ -91,7 +91,8 @@ public class MoveFilesUtil {
                     }
                 }
             }
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return isHasRepeatFiles;
     }
