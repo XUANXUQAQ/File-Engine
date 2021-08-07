d551276dd0b73b0c00478b19a5976806ab769f5a
Xuan Xu <xuanxu233@outlook.com>
2021-08-08 02:20:52 +0800

    添加启动器，删除守护进程设计，采用启动器守护

--- src/main/java/file/engine/event/handler/impl/daemon/StartDaemonEvent.java
+++
@@ -1,8 +0,0 @@
-package file.engine.event.handler.impl.daemon;
-
-public class StartDaemonEvent extends DaemonEvent {
-
-    public StartDaemonEvent(String currentWorkingDir) {
-        super(currentWorkingDir);
-    }
-}
