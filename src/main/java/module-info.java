b3b25d000d1f2414f248610b35df56a8ef7d2fe2
Xuan Xu <xuanxu233@outlook.com>
2021-08-16 16:07:48 +0800

    添加module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module File.Engine {
+    requires java.desktop;
+    requires java.sql;
+    requires TinyPinyin;
+    requires fastjson;
+    requires lombok;
+    requires com.formdev.flatlaf;
+    requires com.formdev.flatlaf.intellijthemes;
+    requires com.sun.jna;
+    requires org.xerial.sqlitejdbc;
+}
