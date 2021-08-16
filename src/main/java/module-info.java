7c75bc5e7f29880991d73c2c67137a22c63e9167
Xuan Xu <xuanxu233@outlook.com>
2021-08-16 18:51:14 +0800

    添加module-info.java文件

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,33 @@ module File.Engine {
     requires java.desktop;
     requires java.sql;
     requires TinyPinyin;
-    requires fastjson;
-    requires lombok;
     requires com.formdev.flatlaf;
     requires com.formdev.flatlaf.intellijthemes;
     requires com.sun.jna;
     requires org.xerial.sqlitejdbc;
+    requires fastjson;
+
+    requires static lombok;
+
+    exports file.engine.configs;
+    exports file.engine.frames;
+    exports file.engine.services;
+    exports file.engine.services.plugin.system;
+    exports file.engine.services.download;
+    exports file.engine.utils;
+    exports file.engine.utils.file;
+    exports file.engine.utils.bit;
+    exports file.engine.utils.clazz.scan;
+    exports file.engine.utils.system.properties;
+
+    opens file.engine.configs;
+    opens file.engine.frames;
+    opens file.engine.services;
+    opens file.engine.services.plugin.system;
+    opens file.engine.services.download;
+    opens file.engine.utils;
+    opens file.engine.utils.file;
+    opens file.engine.utils.bit;
+    opens file.engine.utils.clazz.scan;
+    opens file.engine.utils.system.properties;
 }
