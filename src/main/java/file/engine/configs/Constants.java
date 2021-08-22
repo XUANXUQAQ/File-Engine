aa1c579eb344154376dda74993f1cfb6714c1d73
Xuan Xu <xuanxu233@outlook.com>
2021-08-23 00:37:58 +0800

    修改启动方式

--- src/main/java/file/engine/configs/Constants.java
+++ src/main/java/file/engine/configs/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 
     public static final int MAX_PATTERN_CACHE_NUM = 20;
 
-    public static final String FILE_NAME = "File-Engine-x64.exe";
+    public static final String FILE_NAME = "File-Engine.jar";
     public static final String LAUNCH_WRAPPER_NAME = "File-Engine.exe";
 
     public static final int MAX_SQL_NUM = 5000;
