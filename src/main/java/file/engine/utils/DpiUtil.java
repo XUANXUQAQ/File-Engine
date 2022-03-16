dca69bb13c042c12d437ee82c8775de577348f0e
xuanxuqaq <xuanxu233@outlook.com>
2022-03-16 19:21:23 +0800

    修复一直获取dpi导致cpu占用较高的问题

--- src/main/java/file/engine/utils/DpiUtil.java
+++ src/main/java/file/engine/utils/DpiUtil.java
@@ -6,12 +6,18 @@ import java.io.InputStreamReader;
 
 public class DpiUtil {
 
+    private static volatile double dpi = 1;
+    private static volatile long getDpiTime = System.currentTimeMillis();
+
     public static double getDpi() {
+        if (System.currentTimeMillis() - getDpiTime <= 5000) {
+            return dpi;
+        }
+        getDpiTime = System.currentTimeMillis();
         String proc = "user/getDpi.exe";
-        double dpi = 1;
         try {
             Process exec = Runtime.getRuntime().exec(proc);
-            ProcessUtil.waitForProcess("getDpi.exe");
+            ProcessUtil.waitForProcess("getDpi.exe", 10);
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(exec.getInputStream()))) {
                 String dpiStr = reader.readLine();
                 dpi = Double.parseDouble(dpiStr);
