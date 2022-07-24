b4404a0fe39daded56bfeaf95a89a2d31a2abc82
xuanxuqaq <xuanxu233@outlook.com>
2022-07-24 19:46:29 +0800

    优化getAscII.dll

--- C++/getAscII/getAscII/dllmain.cpp
+++ C++/getAscII/getAscII/dllmain.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 #include <cstring>
 #include "file_engine_dllInterface_GetAscII.h"
-//#define TEST
 
 using namespace std;
-extern "C" __declspec(dllexport) int getAscII(const char* str);
+
+int getAscII(const char* str);
 
 JNIEXPORT jint JNICALL Java_file_engine_dllInterface_GetAscII_getAscII
 (JNIEnv* env, jobject, jstring str)
@@ -14,28 +14,16 @@ JNIEXPORT jint JNICALL Java_file_engine_dllInterface_GetAscII_getAscII
     return getAscII(env->GetStringUTFChars(str, nullptr));
 }
 
-__declspec(dllexport) int getAscII(const char* str)
+int getAscII(const char* str)
 {
-    char _str[260];
-    strcpy_s(_str, str);
-    auto* s = _str;
+    const auto length = strlen(str);
     auto sum = 0;
-    const auto length = strlen(_str);
-    for (auto i = 0; i < length; i++)
+    for (size_t i = 0; i < length; ++i)
     {
-        if (s[i] > 0)
+        if (str[i] > 0)
         {
-            sum += s[i];
+            sum += str[i];
         }
     }
     return sum;
 }
-
-#ifdef TEST
-int main()
-{
-    int asc = getAscII("SANDISK备份");
-    cout << asc << endl;
-    getchar();
-}
-#endif
