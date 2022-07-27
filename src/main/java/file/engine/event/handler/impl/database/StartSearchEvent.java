9375b947f03f7387b195ec73640d91879cb0e429
xuanxuqaq <xuanxu233@outlook.com>
2022-07-27 19:49:09 +0800

    删除nonnull注解

--- src/main/java/file/engine/event/handler/impl/database/StartSearchEvent.java
+++ src/main/java/file/engine/event/handler/impl/database/StartSearchEvent.java
@@ -1,7 +1,6 @@
 package file.engine.event.handler.impl.database;
 
 import file.engine.event.handler.Event;
-import lombok.NonNull;
 
 public class StartSearchEvent extends Event {
 
@@ -9,7 +8,7 @@ public class StartSearchEvent extends Event {
     public final String searchText;
     public final String[] keywords;
 
-    public StartSearchEvent(@NonNull String searchText, @NonNull String[] searchCase, @NonNull String[] keywords) {
+    public StartSearchEvent(String searchText, String[] searchCase, String[] keywords) {
         this.searchCase = searchCase;
         this.searchText = searchText;
         this.keywords = keywords;
