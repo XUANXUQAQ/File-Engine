e676d3dc3d66c50da812ccb68e2a4d3ff084a4b7
xuanxuqaq <xuanxu233@outlook.com>
2022-07-27 21:49:35 +0800

    回滚StartSearchEvent

--- src/main/java/file/engine/event/handler/impl/database/StartSearchEvent.java
+++ src/main/java/file/engine/event/handler/impl/database/StartSearchEvent.java
@@ -1,14 +1,17 @@
 package file.engine.event.handler.impl.database;
 
 import file.engine.event.handler.Event;
+import lombok.NonNull;
+
+import java.util.function.Supplier;
 
 public class StartSearchEvent extends Event {
 
-    public final String[] searchCase;
-    public final String searchText;
-    public final String[] keywords;
+    public final Supplier<String[]> searchCase;
+    public final Supplier<String> searchText;
+    public final Supplier<String[]> keywords;
 
-    public StartSearchEvent(String searchText, String[] searchCase, String[] keywords) {
+    public StartSearchEvent(@NonNull Supplier<String> searchText, @NonNull Supplier<String[]> searchCase, @NonNull Supplier<String[]> keywords) {
         this.searchCase = searchCase;
         this.searchText = searchText;
         this.keywords = keywords;
--- src/main/java/file/engine/services/DatabaseService.java
+++ src/main/java/file/engine/services/DatabaseService.java
@@ -1599,11 +1599,11 @@ public class DatabaseService {
     private static void startSearchEvent(Event event) {
         StartSearchEvent startSearchEvent = (StartSearchEvent) event;
         DatabaseService databaseService = getInstance();
-        databaseService.searchText = startSearchEvent.searchText;
-        databaseService.searchCase = startSearchEvent.searchCase;
+        databaseService.searchText = startSearchEvent.searchText.get();
+        databaseService.searchCase = startSearchEvent.searchCase.get();
         databaseService.isIgnoreCase = databaseService.searchCase == null ||
                 Arrays.stream(databaseService.searchCase).noneMatch(s -> s.equals(Constants.Enums.SearchCase.CASE));
-        String[] _keywords = startSearchEvent.keywords;
+        String[] _keywords = startSearchEvent.keywords.get();
         databaseService.keywords = new String[_keywords.length];
         databaseService.keywordsLowerCase = new String[_keywords.length];
         databaseService.isKeywordPath = new boolean[_keywords.length];
