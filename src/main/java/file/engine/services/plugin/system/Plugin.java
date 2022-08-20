397b7895b3a4290f119cc32c08a5c016cfc8400e
xuanxuqaq <xuanxu233@outlook.com>
2022-08-20 13:44:10 +0800

    修复插件加载失败仍然调用插件方法的问题

--- src/main/java/file/engine/services/plugin/system/Plugin.java
+++ src/main/java/file/engine/services/plugin/system/Plugin.java
@@ -1,5 +1,7 @@
 package file.engine.services.plugin.system;
 
+import lombok.SneakyThrows;
+
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -60,8 +62,17 @@ public class Plugin {
         }
     }
 
+    @SneakyThrows
     @SuppressWarnings("unchecked")
     private <T> T invokeByKey(String key, Object... args) {
+        if (methodHashMap.containsKey(key)) {
+            return (T) methodHashMap.get(key).invoke(instance, args);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T invokeByKeyNoExcept(String key, Object... args) {
         try {
             if (methodHashMap.containsKey(key)) {
                 return (T) methodHashMap.get(key).invoke(instance, args);
@@ -73,25 +84,25 @@ public class Plugin {
     }
 
     public String restoreFileEngineEventHandler() {
-        return invokeByKey("restoreFileEngineEventHandler[]");
+        return invokeByKeyNoExcept("restoreFileEngineEventHandler[]");
     }
 
     public Object[] pollFromEventHandlerQueue() {
-        return invokeByKey("pollFromEventHandlerQueue[]");
+        return invokeByKeyNoExcept("pollFromEventHandlerQueue[]");
     }
 
     public Object[] pollFromEventQueue() {
-        return invokeByKey("pollFromEventQueue[]");
+        return invokeByKeyNoExcept("pollFromEventQueue[]");
     }
 
     public void eventProcessed(Class<?> c, Object eventInstance) {
         String key = "eventProcessed" + Arrays.toString(new Class<?>[]{Class.class, Object.class});
-        invokeByKey(key, c, eventInstance);
+        invokeByKeyNoExcept(key, c, eventInstance);
     }
 
     public void configsChanged(Map<String, Object> configs) {
         String key = "configsChanged" + Arrays.toString(new Class<?>[]{Map.class});
-        invokeByKey(key, configs);
+        invokeByKeyNoExcept(key, configs);
     }
 
     public void loadPlugin(Map<String, Object> configs) {
@@ -101,22 +112,22 @@ public class Plugin {
 
     public void searchBarVisible(String showingMode) {
         String key = "searchBarVisible" + Arrays.toString(new Class<?>[]{String.class});
-        invokeByKey(key, showingMode);
+        invokeByKeyNoExcept(key, showingMode);
     }
 
     public void setCurrentTheme(int defaultColor, int chosenLabelColor, int borderColor) {
         String key = "setCurrentTheme" + Arrays.toString(new Class<?>[]{int.class, int.class, int.class});
-        invokeByKey(key, defaultColor, chosenLabelColor, borderColor);
+        invokeByKeyNoExcept(key, defaultColor, chosenLabelColor, borderColor);
     }
 
     public void clearResultQueue() {
         String key = "clearResultQueue[]";
-        invokeByKey(key);
+        invokeByKeyNoExcept(key);
     }
 
     public int getApiVersion() {
         String key = "getApiVersion[]";
-        Object api = invokeByKey(key);
+        Object api = invokeByKeyNoExcept(key);
         if (api == null) {
             return 1;
         }
@@ -125,17 +136,17 @@ public class Plugin {
 
     public String[] getMessage() {
         String key = "getMessage[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public String pollFromResultQueue() {
         String key = "pollFromResultQueue[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public void textChanged(String text) {
         String key = "textChanged" + Arrays.toString(new Class<?>[]{String.class});
-        invokeByKey(key, text);
+        invokeByKeyNoExcept(key, text);
     }
 
     public void loadPlugin() {
@@ -145,57 +156,57 @@ public class Plugin {
 
     public void unloadPlugin() {
         String key = "unloadPlugin[]";
-        invokeByKey(key);
+        invokeByKeyNoExcept(key);
     }
 
     public void keyPressed(KeyEvent e, String result) {
         String key = "keyPressed" + Arrays.toString(new Class<?>[]{KeyEvent.class, String.class});
-        invokeByKey(key, e, result);
+        invokeByKeyNoExcept(key, e, result);
     }
 
     public void keyReleased(KeyEvent e, String result) {
         String key = "keyReleased" + Arrays.toString(new Class<?>[]{KeyEvent.class, String.class});
-        invokeByKey(key, e, result);
+        invokeByKeyNoExcept(key, e, result);
     }
 
     public void keyTyped(KeyEvent e, String result) {
         String key = "keyTyped" + Arrays.toString(new Class<?>[]{KeyEvent.class, String.class});
-        invokeByKey(key, e, result);
+        invokeByKeyNoExcept(key, e, result);
     }
 
     public void mousePressed(MouseEvent e, String result) {
         String key = "mousePressed" + Arrays.toString(new Class<?>[]{MouseEvent.class, String.class});
-        invokeByKey(key, e, result);
+        invokeByKeyNoExcept(key, e, result);
     }
 
     public void mouseReleased(MouseEvent e, String result) {
         String key = "mouseReleased" + Arrays.toString(new Class<?>[]{MouseEvent.class, String.class});
-        invokeByKey(key, e, result);
+        invokeByKeyNoExcept(key, e, result);
     }
 
     public String getAuthor() {
         String key = "getAuthor[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public ImageIcon getPluginIcon() {
         String key = "getPluginIcon[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public String getOfficialSite() {
         String key = "getOfficialSite[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public String getVersion() {
         String key = "getVersion[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public String getDescription() {
         String key = "getDescription[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public boolean isLatest() throws Exception {
@@ -209,12 +220,12 @@ public class Plugin {
 
     public String getUpdateURL() {
         String key = "getUpdateURL[]";
-        return invokeByKey(key);
+        return invokeByKeyNoExcept(key);
     }
 
     public void showResultOnLabel(String result, JLabel label, boolean isChosen) {
         String key = "showResultOnLabel" + Arrays.toString(new Class<?>[]{String.class, JLabel.class, boolean.class});
-        invokeByKey(key, result, label, isChosen);
+        invokeByKeyNoExcept(key, result, label, isChosen);
     }
 
     /**
