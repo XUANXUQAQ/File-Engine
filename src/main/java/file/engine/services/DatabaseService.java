dd076889cb618ce99a868c46a9e31c85098d6382
xuanxuqaq <xuanxu233@outlook.com>
2022-09-22 13:33:03 +0800

    删除opencl

--- src/main/java/file/engine/services/DatabaseService.java
+++ src/main/java/file/engine/services/DatabaseService.java
@@ -627,27 +627,9 @@ public class DatabaseService {
                 if (shouldStopSearch.get()) {
                     return matchedResultCount;
                 }
-                // 使用OpenCL
-                if (OpenCLMatchUtil.isOpenCLAvailableOnSystem()) {
-                    matchedResultCount += OpenCLMatchUtil.check(tmpQueryResultsCache.toArray(),
-                            searchCase,
-                            isIgnoreCase,
-                            searchText,
-                            keywords,
-                            keywordsLowerCase,
-                            isKeywordPath,
-                            path -> {
-                                if (!tempResultsForEvent.contains(path) && !container.contains(path) && container.add(path)) {
-                                    if (allResultsRecordCounter.incrementAndGet() >= MAX_RESULTS) {
-                                        stopSearch();
-                                    }
-                                }
-                            });
-                } else {
-                    matchedResultCount += tmpQueryResultsCache.stream()
-                            .filter(each -> checkIsMatchedAndAddToList(each, container))
-                            .count();
-                }
+                matchedResultCount += tmpQueryResultsCache.stream()
+                        .filter(each -> checkIsMatchedAndAddToList(each, container))
+                        .count();
             }
         } catch (SQLException e) {
             System.err.println("error sql : " + sql);
@@ -905,19 +887,9 @@ public class DatabaseService {
             if (IsDebug.isDebug()) {
                 System.out.println("从缓存中读取 " + key);
             }
-            if (OpenCLMatchUtil.isOpenCLAvailableOnSystem()) {
-                matchedNum = OpenCLMatchUtil.check(cache.data.toArray(), searchCase, isIgnoreCase, searchText, keywords, keywordsLowerCase, isKeywordPath, path -> {
-                    if (!tempResultsForEvent.contains(path) && !container.contains(path) && container.add(path)) {
-                        if (allResultsRecordCounter.incrementAndGet() >= MAX_RESULTS) {
-                            stopSearch();
-                        }
-                    }
-                });
-            } else {
-                matchedNum = cache.data.stream()
-                        .filter(record -> checkIsMatchedAndAddToList(record, container))
-                        .count();
-            }
+            matchedNum = cache.data.stream()
+                    .filter(record -> checkIsMatchedAndAddToList(record, container))
+                    .count();
         } else {
             //格式化是为了以后的拓展性
             String formattedSql = String.format(sql, "PATH");
