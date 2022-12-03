8a7c2eaf86ef488253a45b904e274eac57cf5b70
xuanxuqaq <xuanxu233@outlook.com>
2022-12-03 17:09:00 +0800

    添加关键字

--- src/main/java/file/engine/frames/SearchBar.java
+++ src/main/java/file/engine/frames/SearchBar.java
@@ -115,7 +115,7 @@ public class SearchBar {
     private volatile long visibleStartTime = 0;  //记录窗口开始可见的事件，窗口默认最短可见时间0.5秒，防止窗口快速闪烁
     private volatile long firstResultStartShowingTime = 0;  //记录开始显示结果的时间，用于防止刚开始移动到鼠标导致误触
     private volatile ArrayList<String> listResults;  //保存从数据库中找出符合条件的记录（文件路径）
-    private ConcurrentLinkedQueue<String> tempResultsFromDatabase;
+    private volatile ConcurrentLinkedQueue<String> tempResultsFromDatabase;
     private volatile String[] searchCase;
     private volatile String searchText = "";
     private volatile String[] keywords;
