aa00b06ffd3eec00ecfc0e0075dc20dbafacb25c
XUANXUQAQ <xuanxusb@outlook.com>
2023-04-03 10:45:44 +0800

    删除不必要启动参数

--- src/main/java/file/engine/MainClass.java
+++ src/main/java/file/engine/MainClass.java
@@ -164,7 +164,6 @@ public class MainClass {
      * -DFile_Engine_Preview=true
      */
     private static void setSystemProperties() {
-        System.setProperty("File_Engine_Preview", "false");
         System.setProperty("file.encoding", "UTF-8");
         System.setProperty("org.sqlite.lib.path", Path.of("user/").toAbsolutePath().toString());
         System.setProperty("org.sqlite.lib.name", "sqliteJDBC.dll");
