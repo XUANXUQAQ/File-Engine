3a3470a6dd733af911ba83d61664a3634662dd32
XUANXUQAQ <xuanxu233@outlook.com>
2023-11-29 22:47:04 +0800

    添加cuda12依赖

--- src/main/java/file/engine/MainClass.java
+++ src/main/java/file/engine/MainClass.java
@@ -345,6 +345,7 @@ public class MainClass {
         checkMd5AndReplace("user/cudaAccelerator.dll", "/win32-native/cudaAccelerator.dll");
         checkMd5AndReplace("user/openclAccelerator.dll", "/win32-native/openclAccelerator.dll");
         checkMd5AndReplace("cudart64_110.dll", "/win32-native/cudart64_110.dll");
+        checkMd5AndReplace("cudart64_12.dll", "/win32-native/cudart64_12.dll");
         checkMd5AndReplace("user/systemThemeInfo.dll", "/win32-native/systemThemeInfo.dll");
         checkMd5AndReplace("user/getDpi.exe", "/win32-native/getDpi.exe");
     }
